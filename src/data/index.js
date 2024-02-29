b0a6d74af71c2c30e144d080eab9fa28aadd6c91
elsabilah1 <elsabilah1@gmail.com>
2024-02-29 14:24:50 +0700

    hide undone

--- src/data/index.js
+++ src/data/index.js
@@ -1,70 +1,70 @@
 export const data = [
   {
-    title: 'Product preview card component',
-    link: '/product-preview-card-comp',
+    title: "Product preview card component",
+    link: "/product-preview-card-comp",
   },
   {
-    title: 'Interactive rating component',
-    link: '/interactive-rating-comp',
+    title: "Interactive rating component",
+    link: "/interactive-rating-comp",
   },
   {
-    title: 'QR code component',
-    link: '/qr-code-comp',
+    title: "QR code component",
+    link: "/qr-code-comp",
   },
   {
-    title: 'NFT preview card component',
-    link: '/nft-preview-card-comp',
+    title: "NFT preview card component",
+    link: "/nft-preview-card-comp",
   },
   {
-    title: 'Order summary component',
-    link: '/order-summary-comp',
+    title: "Order summary component",
+    link: "/order-summary-comp",
   },
   {
-    title: 'Stats preview card component',
-    link: '/stats-preview-card-comp',
+    title: "Stats preview card component",
+    link: "/stats-preview-card-comp",
   },
-  {
-    title: '3-column preview card component',
-    link: '/3-column-preview-card-comp',
-  },
-  {
-    title: 'Profile card component',
-    link: '/profile-card-comp',
-  },
-  {
-    title: 'FAQ accordion card',
-    link: '/faq-accordion-card',
-  },
-  {
-    title: 'Social proof section',
-    link: '/social-proof-section',
-  },
-  {
-    title: 'Article preview component',
-    link: '/article-preview-comp',
-  },
-  {
-    title: 'Four card feature section',
-    link: '/four-card-feature-section',
-  },
-  {
-    title: 'Base Apparel coming soon page',
-    link: '/base-apparel-coming-soon-page',
-  },
-  {
-    title: 'Intro component with sign-up form',
-    link: '/intro-component-with-sign-up-form',
-  },
-  {
-    title: 'Single price grid component',
-    link: '/single-price-grid-comp',
-  },
-  {
-    title: 'Ping single column coming soon page',
-    link: '/ping-single-column-coming-soon-page',
-  },
-  {
-    title: 'Huddle landing page with a single introductory section',
-    link: '/huddle-landing-page-with-a-single-introductory-section',
-  },
-]
+  // {
+  //   title: '3-column preview card component',
+  //   link: '/3-column-preview-card-comp',
+  // },
+  // {
+  //   title: 'Profile card component',
+  //   link: '/profile-card-comp',
+  // },
+  // {
+  //   title: 'FAQ accordion card',
+  //   link: '/faq-accordion-card',
+  // },
+  // {
+  //   title: 'Social proof section',
+  //   link: '/social-proof-section',
+  // },
+  // {
+  //   title: 'Article preview component',
+  //   link: '/article-preview-comp',
+  // },
+  // {
+  //   title: 'Four card feature section',
+  //   link: '/four-card-feature-section',
+  // },
+  // {
+  //   title: 'Base Apparel coming soon page',
+  //   link: '/base-apparel-coming-soon-page',
+  // },
+  // {
+  //   title: 'Intro component with sign-up form',
+  //   link: '/intro-component-with-sign-up-form',
+  // },
+  // {
+  //   title: 'Single price grid component',
+  //   link: '/single-price-grid-comp',
+  // },
+  // {
+  //   title: 'Ping single column coming soon page',
+  //   link: '/ping-single-column-coming-soon-page',
+  // },
+  // {
+  //   title: 'Huddle landing page with a single introductory section',
+  //   link: '/huddle-landing-page-with-a-single-introductory-section',
+  // },
+];
